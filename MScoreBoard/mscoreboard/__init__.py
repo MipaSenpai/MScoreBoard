b09ebb75d49efe9e3dc59cfe8d8bac0c16355ebf
Mipa <156837662+MipaSenpai@users.noreply.github.com>
2024-01-17 19:01:47 +0300

    Delete MScoreBoard directory

--- MScoreBoard/mscoreboard/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-from . import scoreboard as scoreboard
-
-if TYPE_CHECKING:
-    from llpy import logger
-
-logger.info('Плагин успешно включен.')
-
--- MScoreBoard/mscoreboard/config.py
+++
@@ -1,9 +0,0 @@
-name_server = 'Название сервера'
-
-player_name = 'Имя: '
-
-online = 'Онлайн: '
-max_online = '/100'
-
-kills = 'Убийств: '
-deaths = 'Смертей: '
